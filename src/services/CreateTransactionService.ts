e6cfc6acbca443dc171051c0c8f1a6486348fb97
JhonatasFarias <jhonatas.s.farias@gmail.com>
2020-09-25 16:06:47 -0300

    db upload - delete & import CSV - challenge 06 ok

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,9 +1,10 @@
-import {getCustomRepository} from 'typeorm'
+import {getCustomRepository, getRepository} from 'typeorm'
 
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
 
 import TransactionRepository from '../repositories/TransactionsRepository'
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
 interface RequestDTO{
   title: string
@@ -15,13 +16,41 @@ interface RequestDTO{
 
 class CreateTransactionService {
   
-  public async execute({title, value, type, category}: RequestDTO): Promise<Transaction> {
+  public async execute({
+    title, 
+    value, 
+    type, 
+    category
+  }: RequestDTO): Promise<Transaction> {    
     const transactionRepository = getCustomRepository(TransactionRepository)
+    const categoryRepository = getRepository(Category)
+
+    const {total} = await transactionRepository.getBalance()
+
+    if(type === 'outcome' && total < value){
+      throw new AppError('You do not have enough balance')
+    }
+
+
+    let transactionCategory = await categoryRepository.findOne({
+      where:{
+        title:category
+      }
+    })
+
+    if(!transactionCategory){
+      transactionCategory = categoryRepository.create({
+        title:category
+      })
+
+      await categoryRepository.save(transactionCategory)
+    }
 
     const transaction = transactionRepository.create({
       title,
       value,
       type,
+      category: transactionCategory,
     })
 
     await transactionRepository.save(transaction)
